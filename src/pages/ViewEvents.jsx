63786d0086441a04915a0e9d6157bdfbd958cc5f
UsminDhakal <usmindhakal123456789@gmail.com>
2024-06-13 00:03:21 +0545

    AddPost section2.0

--- src/pages/ViewEvents.jsx
+++ src/pages/ViewEvents.jsx
@@ -7,10 +7,10 @@ function ViewEvents() {
   return (
     <div className='h-screen w-screen bg-slate-200'>
         <Navbar />
-        {/* <div className="grid grid-cols-4 gap-10 w-[100vw] px-10 mt-10">
+        <div className="grid grid-cols-4 gap-10 w-[100vw] px-10 mt-10">
             <EventsList />
-        </div> */}
-        <EventPost />
+        </div>
+        {/* <EventPost /> */}
 
 
 
